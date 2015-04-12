1854a70175aa0badc3c717e15004dee27f2814fe
sunny200745 <sunny.200745@gmail.com>
2015-04-12 21:24:39 +0530

    Initial setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name trainingExamplesApp
+ * @description
+ * # trainingExamplesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('trainingExamplesApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trainingExamplesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trainingExamplesApp
+ */
+angular.module('trainingExamplesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
