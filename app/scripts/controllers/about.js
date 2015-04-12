170ed65ca774310872ec22a4fd3bd7ef7f5f2c44
sunny200745 <sunny.200745@gmail.com>
2015-04-12 21:54:57 +0530

    Directive : ngRepeat

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trainingExamplesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trainingExamplesApp
+ */
+angular.module('trainingExamplesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.repeatData = ["1","4","5","6","7"]
+  });
